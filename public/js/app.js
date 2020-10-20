7ff44f3b2491019b3150bfab30d570a48cadc026
maneanirudha <maneanirudha@gmail.com>
2020-10-20 09:39:47 +0530

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+
+
+console.log('Client side javascript file is loaded!')
+
+
+const formin = document.querySelector('form')
+const search  = document.querySelector('input')
+const messageone = document.querySelector('#message1')
+const messagetwo = document.querySelector('#message2')
+
+
+
+
+formin.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    const loc = search.value
+    fetch('http://api.weatherapi.com/v1/current.json?key=d3cc2b169c3e4bdf93d60535201710&q='+loc).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            messageone.textContent = data.error.message
+        }else{
+            messageone.textContent = data.location.name
+            messagetwo.textContent = data.current.temp_c
+            // console.log(data.location.name)
+            // console.log(data.current.temp_c)
+        }
+    })
+})
+})
---
+++ src/utils/location.js
@@ -0,0 +1,21 @@
+//const chalk = require('chalk')
+const request = require('request')
+
+const location = (address, callback) => {
+    const url = 'http://api.weatherapi.com/v1/current.json?key=d3cc2b169c3e4bdf93d60535201710&q='+address
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                location: body.location.name+','+body.location.region+','+body.location.country,
+                temp: 'Temp is: '+body.current.temp_c
+            })
+        }
+    })
+}
+
+module.exports = location
